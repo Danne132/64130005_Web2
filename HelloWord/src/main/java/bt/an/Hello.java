e32bce2d75829995e753bf4f18abb94be9a71faa
Danne132 <163326801+Danner45@users.noreply.github.com>
2025-02-13 08:15:59 +0700

    test doGet

--- HelloWord/src/main/java/bt/an/Hello.java
+++ HelloWord/src/main/java/bt/an/Hello.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class Hello
@@ -27,7 +28,9 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>Hello?</h1>");
 	}
 
 	/**
