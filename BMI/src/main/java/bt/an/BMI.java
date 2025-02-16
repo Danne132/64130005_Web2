49ac94d2df3850d47c9d37de91c86251b4a1388f
Danne132 <163326801+Danner45@users.noreply.github.com>
2025-02-16 23:45:48 +0700

    tạo project tính BMI

---
+++ BMI/src/main/java/bt/an/BMI.java
@@ -0,0 +1,53 @@
+package bt.an;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class BMI
+ */
+@WebServlet("/BMI")
+public class BMI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BMI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter print = response.getWriter();
+		print.append("BMI Calculator");
+		String inputName = "<form method = POST action=\"/BMI/BMI\">"
+				+ "<lable> Weight: </lable>"
+				+ "<input type = \"text\" name =\"weight\">"
+				+ "<lable> Height: </lable>"
+				+ "<input type = \"text\" name =\"height\">"
+				+ "<input type = \"submit\" value =\"Send\">"
+				+ "</form>";
+		print.append(inputName);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
