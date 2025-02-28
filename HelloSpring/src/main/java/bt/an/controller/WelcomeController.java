308c0ac20979a04628b9bd70f3d3ab50fbcc3669
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-02-28 09:56:00 +0700

    update

--- HelloSpring/src/main/java/bt/an/controller/WelcomeController.java
+++ HelloSpring/src/main/java/bt/an/controller/WelcomeController.java
@@ -1,12 +1,13 @@
-package bt.an;
+package bt.an.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-	@RequestMapping("/welcome")
-	public String welcome() {
+	@RequestMapping("/viewWelcome")
+	public String viewWelcome(ModelMap model) {
 		return "viewWelcome";
 	}
 }
