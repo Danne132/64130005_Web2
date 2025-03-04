e569a7c79dc74c8cb460299992c84c632a0f74dc
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-04 10:06:35 +0700

    thêm file html

--- HelloSpringBoot/src/main/java/com/duyan/HelloSpringBoot/HelloController.java
+++ HelloSpringBoot/src/main/java/com/duyan/HelloSpringBoot/HelloController.java
@@ -1,5 +1,18 @@
 package com.duyan.HelloSpringBoot;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Controller //Để chương trình xác định đây là một controller
+public class HelloController {
+	// Mỗi controller có một action
+	// Cần có một URL tương ứng để gọi cho action này
+	@RequestMapping("/hello") //Người dùng request đến địa chỉ /hello
+	public String helloBoot() {
+		// Code xử lý... 
+		return "helloView";
+	}
 }
