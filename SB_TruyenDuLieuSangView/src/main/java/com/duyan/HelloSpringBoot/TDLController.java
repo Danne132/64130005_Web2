2052c2f060ecfb0778cc5f7ca54100d2d959c81e
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-09 19:50:49 +0700

    update code

--- SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/TDLController.java
+++ SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/TDLController.java
@@ -1,5 +1,19 @@
 package com.duyan.HelloSpringBoot;
 
-public class TDLController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class TDLController {
+	
+	@GetMapping("/")
+	public String getMethodName(ModelMap model) {
+		model.addAttribute("MSSV", "64130005");
+		model.addAttribute("HoTen", "Hoàng Duy An");
+		model.addAttribute("NamSinh", "2004");
+		model.addAttribute("GioiTinh", "Nam");
+		return new String();
+	}
+	
 }
