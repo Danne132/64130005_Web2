c26311d5200fb4bac5e77e6ddff33d378d82d91c
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-11 10:31:38 +0700

    Done code

--- SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/model/SinhVien.java
+++ SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/model/SinhVien.java
@@ -1,8 +1,8 @@
 package com.duyan.HelloSpringBoot.model;
 
 public class SinhVien {
-	String mssv, hoTen, gioiTinh;
-	int namSinh;
+	public String mssv, hoTen, gioiTinh;
+	public int namSinh;
 	
 	public SinhVien(String mssv, String hoTen, String gioiTinh, int namSinh){
 		this.mssv = mssv;
