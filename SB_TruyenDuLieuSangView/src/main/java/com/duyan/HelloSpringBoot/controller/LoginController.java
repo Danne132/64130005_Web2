5137374da3efac7e14655210d9f556cafb254db4
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-13 16:28:44 +0700

    update code

--- SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
+++ SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
@@ -1,5 +1,24 @@
 package com.duyan.HelloSpringBoot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
 public class LoginController {
 
+	@GetMapping("/login")
+	public String getMethod() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String login(ModelMap model, HttpServletRequest request) {
+		String email = request.getParameter("email");
+		
+		return "index";
+	}
 }
