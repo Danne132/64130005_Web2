b80c1c427d049202fd5ce9a3109f3ec313f0f9ab
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-13 14:50:36 +0700

    up code BMI

---
+++ BMI_LayDiemCong/src/main/java/bt/duyan/BMIDiemCong/controller/BmiLayDiemCongApplication.java
@@ -0,0 +1,13 @@
+package bt.duyan.BMIDiemCong.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiLayDiemCongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiLayDiemCongApplication.class, args);
+	}
+
+}
---
+++ BMI_LayDiemCong/src/test/java/bt/duyan/BMIDiemCong/BmiLayDiemCongApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.duyan.BMIDiemCong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiLayDiemCongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
