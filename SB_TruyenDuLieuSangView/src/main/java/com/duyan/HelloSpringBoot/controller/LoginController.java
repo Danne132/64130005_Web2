bd9f2029c062ce963916fe3bc29d629f6b223067
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-13 16:35:20 +0700

    update code

--- SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
+++ SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.duyan.HelloSpringBoot.model.SinhVien;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -18,7 +20,11 @@ public class LoginController {
 	@PostMapping("/login")
 	public String login(ModelMap model, HttpServletRequest request) {
 		String email = request.getParameter("email");
-		
+		String pass = request.getParameter("password");
+		if(!(email.equals("duyanhoang05@gmail.com") && pass.equals("12345"))) {
+			model.addAttribute("message", "Email hoặc mật khẩu không đúng!");
+			return "login";
+		}
 		return "index";
 	}
 }
