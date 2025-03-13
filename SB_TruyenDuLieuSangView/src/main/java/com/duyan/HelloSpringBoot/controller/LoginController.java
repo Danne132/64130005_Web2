abbef9a3a209c29e28854f17ebf423a58fe45765
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-13 20:42:28 +0700

    done bài login

--- SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
+++ SB_TruyenDuLieuSangView/src/main/java/com/duyan/HelloSpringBoot/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.duyan.HelloSpringBoot.controller;
 
+import javax.swing.text.html.FormSubmitEvent.MethodType;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.duyan.HelloSpringBoot.model.SinhVien;
 
@@ -13,18 +17,21 @@ import jakarta.servlet.http.HttpServletRequest;
 public class LoginController {
 
 	@GetMapping("/login")
-	public String getMethod() {
+	public String getMethod(ModelMap model) {
+		model.addAttribute("message", "");
 		return "login";
 	}
 	
-	@PostMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(ModelMap model, HttpServletRequest request) {
 		String email = request.getParameter("email");
 		String pass = request.getParameter("password");
-		if(!(email.equals("duyanhoang05@gmail.com") && pass.equals("12345"))) {
+		if(!(email.equals("duyanhoang05") && pass.equals("12345"))) {
 			model.addAttribute("message", "Email hoặc mật khẩu không đúng!");
 			return "login";
 		}
+		SinhVien sVien = new SinhVien("64130005", "Hoàng Duy An", "Nam", 2004);
+		model.addAttribute("sv", sVien);
 		return "index";
 	}
 }
