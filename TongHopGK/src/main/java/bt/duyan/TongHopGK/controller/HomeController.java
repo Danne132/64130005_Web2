c3835c04554e15ef15c2e0b3736d7136ca83db30
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-25 11:13:40 +0700

    update code

--- TongHopGK/src/main/java/bt/duyan/TongHopGK/controller/HomeController.java
+++ TongHopGK/src/main/java/bt/duyan/TongHopGK/controller/HomeController.java
@@ -6,11 +6,23 @@ import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.duyan.HelloSpringBoot.model.SinhVien;
 
 import bt.duyan.TongHopGK.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
+	ArrayList<Student> sinhViens = new ArrayList<Student>(Arrays.asList(
+			new Student("64131234", "Hà Thị Kiều Ngân", 8.5),
+			new Student("64130005", "Hoàng Duy An", 9.0),
+			new Student("64130004", "Dương Thị Ánh Hồng", 8.6),
+			new Student("64130003", "Phạm Trung Kiên", 7.7)
+			));
+	
 	@GetMapping("/")
 	public String toHome() {
 		return "index";
@@ -23,12 +35,6 @@ public class HomeController {
 	
 	@GetMapping("/list")
 	public String toList(ModelMap model) {
-		ArrayList<Student> sinhViens = new ArrayList<Student>(Arrays.asList(
-				new Student("64131234", "Hà Thị Kiều Ngân", 8.5),
-				new Student("64130005", "Hoàng Duy An", 9.0),
-				new Student("64130004", "Dương Thị Ánh Hồng", 8.6),
-				new Student("64130003", "Phạm Trung Kiên", 7.7)
-				));
 		model.addAttribute("svs", sinhViens);
 		return "list";
 	}
@@ -36,5 +42,16 @@ public class HomeController {
 	@GetMapping("/addNew")
 	public String toNew(ModelMap model) {
 		return "newStudent";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String login(ModelMap model, HttpServletRequest request) {
+		String mssv = request.getParameter("mssv");
+		String hoTen = request.getParameter("hoTen");
+		double diemTB = Double.parseDouble(request.getParameter("diemTB"));
+		Student student = new Student(mssv, hoTen, diemTB);
+		sinhViens.add(student);
+		model.addAttribute("sv", sinhViens);
+		return "list";
 	}
 }
