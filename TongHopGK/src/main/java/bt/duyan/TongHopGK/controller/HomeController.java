57fb51e39768bf8eaf298df8cb0b501147c333db
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-25 10:13:50 +0700

    update giao diện about

---
+++ TongHopGK/src/main/java/bt/duyan/TongHopGK/controller/HomeController.java
@@ -0,0 +1,17 @@
+package bt.duyan.TongHopGK.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String toHome() {
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String toAbout() {
+		return "about";
+	}
+}
