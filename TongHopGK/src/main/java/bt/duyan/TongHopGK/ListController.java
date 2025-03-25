158a3361fd686fe17593955af15e33238b3bfd59
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-03-25 10:34:34 +0700

    update giao diện ds sinh viên

---
+++ TongHopGK/src/main/java/bt/duyan/TongHopGK/ListController.java
@@ -0,0 +1,8 @@
+package bt.duyan.TongHopGK;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ListController {
+	
+}
--- TongHopGK/src/main/java/bt/duyan/TongHopGK/controller/HomeController.java
+++ TongHopGK/src/main/java/bt/duyan/TongHopGK/controller/HomeController.java
@@ -1,7 +1,13 @@
 package bt.duyan.TongHopGK.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import bt.duyan.TongHopGK.model.Student;
 
 @Controller
 public class HomeController {
@@ -13,5 +19,17 @@ public class HomeController {
 	@GetMapping("/about")
 	public String toAbout() {
 		return "about";
+	}
+	
+	@GetMapping("/list")
+	public String toList(ModelMap model) {
+		ArrayList<Student> sinhViens = new ArrayList<Student>(Arrays.asList(
+				new Student("64131234", "Hà Thị Kiều Ngân", 8.5),
+				new Student("64130005", "Hoàng Duy An", 9.0),
+				new Student("64130004", "Dương Thị Ánh Hồng", 8.6),
+				new Student("64130003", "Phạm Trung Kiên", 7.7)
+				));
+		model.addAttribute("svs", sinhViens);
+		return "list";
 	}
 }
