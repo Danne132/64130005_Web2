e0d0d007d2707e669c2dec40f682364ae4832f0b
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-01 10:48:39 +0700

    update code

--- HoangDuyAn_ThiGK/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/GKController.java
+++ HoangDuyAn_ThiGK/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/GKController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import thiGK.ntu64130005.HoangDuyAn_ThiGK.model.Page;
+import thiGK.ntu64130005.HoangDuyAn_ThiGK.model.Post;
 
 @Controller
 public class GKController {
@@ -16,7 +17,12 @@ public class GKController {
 			new Page(1, "Home", "home, index", "Đây là trang chủ", 0),
 			new Page(2, "About", "about, company", "About Us Page", 1)
 			));
-	
+	ArrayList<Post> posts = new ArrayList<>(Arrays.asList(
+            new Post("1", "Bài viết 1", "Nội dung bài viết 1", "C1"),
+            new Post("2", "Bài viết 2", "Nội dung bài viết 2", "C2"),
+            new Post("3", "Bài viết 3", "Nội dung bài viết 3", "C3")
+        ));
+
 	
 	@GetMapping("/")
 	public String getDashBoard() {
@@ -50,4 +56,27 @@ public class GKController {
 	public String getNewPage() {
         return "addpage";
 	}
+	
+	@GetMapping("/post/all")
+	public String getAllPost(ModelMap model) {
+		model.addAttribute("posts", posts);
+        return "pagelists";
+	}
+	
+	@GetMapping("/post/{id}")
+    public String viewPost(@PathVariable int id, ModelMap model) {
+        for (Page page : pages) {
+            if (page.id == id) {
+                model.addAttribute("page", page);
+                return "viewpage";
+            }
+        }
+        return "redirect:/dashboard/postlists";
+    }
+	
+	@GetMapping("/post/delete/{id}")
+    public String deletePost(@PathVariable int id) {
+        pages.removeIf(page -> page.id == id);
+        return "redirect:/dashboard/postlists";
+    }
 }
