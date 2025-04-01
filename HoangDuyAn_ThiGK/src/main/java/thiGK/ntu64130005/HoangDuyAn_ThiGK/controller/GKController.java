91c535a3e7eb08146f920dc1353c19834c0b19d8
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-01 10:27:02 +0700

    update code

--- HoangDuyAn_ThiGK/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/GKController.java
+++ HoangDuyAn_ThiGK/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/GKController.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import thiGK.ntu64130005.HoangDuyAn_ThiGK.model.Page;
 
 @Controller
 public class GKController {
-	ArrayList<Page> sinhViens = new ArrayList<Page>(Arrays.asList(
+	ArrayList<Page> pages = new ArrayList<Page>(Arrays.asList(
 			new Page(1, "Home", "home, index", "Đây là trang chủ", 0),
 			new Page(2, "About", "about, company", "About Us Page", 1)
 			));
@@ -21,7 +23,20 @@ public class GKController {
 		return "index";
 	}
 	
-	@GetMapping("/page/all"){
-		return 
+	@GetMapping("/page/all")
+	public String getAllPage(ModelMap model) {
+		model.addAttribute("pages", pages);
+        return "pagelists";
 	}
+	
+	@GetMapping("/page/{id}")
+    public String viewPage(@PathVariable int id, ModelMap model) {
+        for (Page page : pages) {
+            if (page.id == id) {
+                model.addAttribute("page", page);
+                return "viewpage";
+            }
+        }
+        return "redirect:/dashboard/pagelist";
+    }
 }
