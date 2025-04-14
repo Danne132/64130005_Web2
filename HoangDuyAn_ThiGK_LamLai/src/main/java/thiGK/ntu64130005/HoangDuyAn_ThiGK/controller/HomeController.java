3374821ec1c0c4ec78e9a9c041405aa00742c5a1
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-14 08:32:43 +0700

    update code

--- HoangDuyAn_ThiGK_LamLai/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/HomeController.java
+++ HoangDuyAn_ThiGK_LamLai/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/controller/HomeController.java
@@ -1,14 +1,39 @@
 package thiGK.ntu64130005.HoangDuyAn_ThiGK.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import thiGK.ntu64130005.HoangDuyAn_ThiGK.model.Page;
+import thiGK.ntu64130005.HoangDuyAn_ThiGK.model.Post;
 
 @Controller
 public class HomeController {
+	ArrayList<Page> pages = new ArrayList<Page>(Arrays.asList(
+			new Page(1, "Home", "home, index", "Đây là trang chủ", 0),
+			new Page(2, "About", "about, company", "About Us Page", 1)
+			));
+	ArrayList<Post> posts = new ArrayList<>(Arrays.asList(
+            new Post("1", "Bài viết 1", "Nội dung bài viết 1", "C1"),
+            new Post("2", "Bài viết 2", "Nội dung bài viết 2", "C2"),
+            new Post("3", "Bài viết 3", "Nội dung bài viết 3", "C3")
+        ));
 	@GetMapping("/")
 	public String getHome() {
 		return "home";
 	}
 	
 	@GetMapping("/page/all")
+	public String getAllPage(ModelMap model) {
+		model.addAttribute("pages", pages);
+        return "pagelists";
+	}
+	
+	@GetMapping("/page/new")
+	public String getNewPage() {
+        return "addpage";
+	}
 }
--- HoangDuyAn_ThiGK_LamLai/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/model/Page.java
+++ HoangDuyAn_ThiGK_LamLai/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/model/Page.java
@@ -1,5 +1,14 @@
 package thiGK.ntu64130005.HoangDuyAn_ThiGK.model;
 
 public class Page {
-
+	public String pageName, keyword, content;
+	public int id, parentPageId;
+	
+	public Page(int id, String pageName, String keyword, String content, int parentPageId) {
+		this.id = id;
+		this.pageName = pageName;
+		this.keyword = keyword;
+		this.content = content;
+		this.parentPageId = parentPageId;
+	}
 }
