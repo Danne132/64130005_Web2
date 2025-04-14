7805835cdc5473f4738bcb320eead19e5f9f246e
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-14 07:55:48 +0700

    Làm lại bài thi gk

---
+++ HoangDuyAn_ThiGK_LamLai/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGK/HoangDuyAnThiGkLamLaiApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130005.HoangDuyAn_ThiGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangDuyAnThiGkLamLaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoangDuyAnThiGkLamLaiApplication.class, args);
+	}
+
+}
