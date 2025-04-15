a58d7b3de763fcf8374a25b0e92a5040ba02632a
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-15 21:24:10 +0700

    Làm lại đề 2

---
+++ HoangDuyAn_ThiGK_LamLaiDe2/src/main/java/thiGK/ntu64130005/HoangDuyAn_ThiGKDe2/HoangDuyAnThiGkLamLaiDe2Application.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130005.HoangDuyAn_ThiGKDe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangDuyAnThiGkLamLaiDe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoangDuyAnThiGkLamLaiDe2Application.class, args);
+	}
+
+}
---
+++ HoangDuyAn_ThiGK_LamLaiDe2/src/test/java/thiGK/ntu64130005/HoangDuyAn_ThiGKDe2/HoangDuyAnThiGkLamLaiDe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130005.HoangDuyAn_ThiGKDe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangDuyAnThiGkLamLaiDe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
