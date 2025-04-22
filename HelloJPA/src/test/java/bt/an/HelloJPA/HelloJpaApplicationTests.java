0eac56abb8903beb0bce3d514f7bc56b203bca72
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-22 10:05:14 +0700

    Tạo file demo CSDL dùng JPA

---
+++ HelloJPA/src/test/java/bt/an/HelloJPA/HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.an.HelloJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
