c1c66d22dd9a698f1f0b0b7d3b732f19b89f6bc5
Danne132 <163711956+Danne132@users.noreply.github.com>
2025-04-22 11:29:45 +0700

    update code

--- HelloJPA/src/main/java/bt/an/HelloJPA/controllers/CustomerController.java
+++ HelloJPA/src/main/java/bt/an/HelloJPA/controllers/CustomerController.java
@@ -1,10 +1,14 @@
 package bt.an.HelloJPA.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bt.an.HelloJPA.models.Customer;
 import bt.an.HelloJPA.respositories.ICustomer;
 
 @Controller
@@ -14,8 +18,10 @@ public class CustomerController {
 	//Các action method
 	
 	@GetMapping("/customer/all")
-	public String getAll() {
-		myCustomer.count();
+	public String getAll(ModelMap model) {
+		ArrayList<Customer> ds = new ArrayList<Customer>();
+		ds.addAll(myCustomer.findAll());
+		model.addAttribute("ds", ds);
 		return null;
 	}
 }
--- HelloJPA/src/main/java/bt/an/HelloJPA/respositories/ICustomer.java
+++ HelloJPA/src/main/java/bt/an/HelloJPA/respositories/ICustomer.java
@@ -1,9 +1,11 @@
 package bt.an.HelloJPA.respositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import bt.an.HelloJPA.models.Customer;
 
+@Repository
 public interface ICustomer extends JpaRepository<Customer, Integer>{
 	
 }
